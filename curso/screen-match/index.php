bdbfa9be53848fe2ef0a0e7c51a3ae62db194d89
MatheusFVieira <160733404+MatheusFVieira@users.noreply.github.com>
2025-03-18 12:19:19 -0300

    Continuação projeto POO

--- curso/screen-match/index.php
+++ curso/screen-match/index.php
@@ -1,61 +1,43 @@
 <?php
 
-require __DIR__ . "/src/funcoes.php";
+require __DIR__ . "/src/Modelo/Titulo.php";
+require __DIR__ . "/src/Modelo/Genero.php";
+require __DIR__ . "/src/Modelo/Filme.php";
+require __DIR__ . "/src/Modelo/Serie.php";
 
-echo "Bem-vindo(a) ao screen match!\n";
+echo "Bem-vindo(a) ao ScreenMatch\n";
 
-$nomeFilme = "Top Gun - Maverick";
-
-$anoLancamento = 2022;
-
-$quantidadeDeNotas = $argc - 1;
-$notas = [];
-
-for ($contador = 1; $contador < $argc; $contador++) {
-    $notas[] = (float) $argv[$contador];
-}
-
-$notaFilme = array_sum($notas) / $quantidadeDeNotas;
-$planoPrime = true;
-
-$incluidoNoPlano = incluidoNoPlano($planoPrime, $anoLancamento);
-
-echo "Nome do filme: " . $nomeFilme . "\n";
-echo "Nota do filme: $notaFilme\n";
-echo "Ano de lançamento: $anoLancamento\n";
-
-exibeMensagemLancamento($anoLancamento);
-
-$genero = match ($nomeFilme) {
-    "Top Gun - Maverick" => "ação",
-    "Thor: Ragnarok" => "super-herói",
-    "Se beber não case" => "comédia",
-    default => "gênero desconhecido",
-};
-
-echo "O gênero do filme é: $genero\n";
-
-$filme = criaFilme(
-    nome: "Thor: Ragnarok", 
-    anoLancamento: 2021, 
-    nota: 7.8, 
-    genero:"super-herói"
+$filme = new Filme(
+    'Thor - Ragnarok',
+    2021,
+    Genero::SuperHeroi,
+    180,
 );
-    
 
-echo $filme["ano"];
+$filme->avalia(10);
+$filme->avalia(5.2);
+$filme->avalia(6.5);
+$filme->avalia(8);
+$filme->avalia(7);
 
-var_dump($notas);
-sort($notas);
-var_dump($notas);
-$menorNota = min($notas);
-var_dump($menorNota);
+var_dump($filme);
+echo $filme->media() . "\n";
 
-var_dump($filme['nome']);
-$posicaoDoisPontos = strpos($filme['nome'], ':');
-var_dump($posicaoDoisPontos);
+echo $filme->anoLancamento . "\n";
 
-var_dump(substr($filme['nome'], 0, $posicaoDoisPontos));
+$serie = new Serie(
+    'Lost',
+    2007,
+    Genero::Drama,
+    10,
+    20,
+    30,
+);
 
-$filmeComoStringJson = json_encode($filme);
-file_put_contents(__DIR__ . '/filme.json', $filmeComoStringJson);
+echo $serie->anoLancamento . "\n";
+
+$serie->avalia(8);
+
+echo $serie->media() . "\n";
+
+var_dump($serie);
